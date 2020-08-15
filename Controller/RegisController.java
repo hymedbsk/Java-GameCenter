d577b79aa9830026df7d4ab8afd611b7f7a41e52
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-08-15 22:04:04 +0200

    Inscription 1.0 MVC
    Inscription MVC sans bdd

---
+++ Controller/RegisController.java
@@ -0,0 +1,57 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import Model.RegisModel;
+import View.RegisView;
+
+
+public class RegisController {
+
+	private RegisView regisView;
+	private RegisModel regisModel;
+	
+	public RegisController(RegisView regisView, RegisModel regisModel ) {
+		
+		this.regisView = regisView;
+		this.regisModel = regisModel;		
+		this.regisView.RegisListener(new RegisListener());
+		
+	}
+	
+	class RegisListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			System.out.println("oi");
+			String Nom, Prenom, Pseudo, Password, Password2;
+			
+			   try {
+				
+				Pseudo = regisView.getTxtPseudo();								
+				Prenom = regisView.getTxtPrenom();
+				Nom = regisView.getTxtNom();
+				Password = regisView.getTxtMotDePasse();	
+				Password2 = regisView.getTxtMotDePasse2();
+				if(Password.equals(Password2)) {
+					
+					System.out.println("Les mots de passe ne correspondent pas.");
+				}else {
+					JOptionPane.showMessageDialog(null,"Les mots de passe ne correspondent pas.");
+					System.out.println("Les mots de passe ne correspondent pas.");
+				}
+				
+			}catch(NumberFormatException ex){
+				
+				System.out.println(ex);
+				
+			}
+		}
+		
+	}
+	
+}
